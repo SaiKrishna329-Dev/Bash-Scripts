364a2db51f8caf9f807904b73d2107a0e27bf176
SaiKrishna329-Dev <gsaikrishna1496@gmail.com>
2025-10-19 12:34:35 +0530

    Bash scripts concepts

--- Loops/for_loop-01.sh
+++ Loops/for_loop-01.sh
@@ -2,14 +2,14 @@
 
 # Print numbers from 1 to 10 using a for loop.
 
-#!/bin/bash
+!/bin/bash
  
 for (( i=1; i<=10; i++ ))
 do 
   echo "output: $i"
 done  
 
-#Print even numbers between 1 to 20.
+# #Print even numbers between 1 to 20.
 
 max=20
 
@@ -21,4 +21,21 @@ do
 done   
 
 
-#
+#Sum all numbers from 1 to N (user input)
+
+read -p "enter the num: " num
+
+for (( i=1; i<=num; i++ ))
+do 
+
+  sum=$((sum+i))
+  
+done  
+   echo "sum of the all numbers till $num: $sum"
+   
+
+# Sum all numbers from 1 to N using arithmetic formula
+read -p "enter the num: " num   
+
+sum=$((num*(num+1)/2))
+ echo "sum of the all numbers till $num: $sum"
---
+++ Loops/is_txt_file.sh
@@ -0,0 +1,15 @@
+# Loop through all .txt files in a directory and display the number of lines in each.
+
+#!/bin/bash
+
+dir="/Users/gunduboina.saikrishna/Repos/DevOps"
+
+for file in "$dir"/*.txt
+do 
+  if [[ -e $file ]]; then
+    
+    num=$(wc -l < $file)
+    echo "number of lines in $file:$num"
+  fi
+done      
+  
